ef047ed4f75c464e1326663c446a7b7b4772cc5d
Weerapat1993 <weerapat.thawatchoketawee@gmail.com>
2024-09-04 20:51:33 +0700

    Update v1.0.1

---
+++ assets/FormInput-A2L7SCI2.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-BMCNW8jw.js";import{I as a}from"./InputLabel-ofGMDwKr.js";import{T as e,I as o}from"./TextInput-DKrkv53f.js";const x=({label:m,errorMessage:r,...t})=>s.jsxs("div",{children:[s.jsx(a,{htmlFor:t.name,value:m}),s.jsx(e,{className:"mt-1 block w-full",...t}),s.jsx(o,{className:"mt-2",message:r})]});export{x as F};
