a2ce585acf3ee642ef179ba2f112ba5f6f1f6a2f
Weerapat1993 <weerapat.thawatchoketawee@gmail.com>
2024-09-04 17:59:15 +0700

    first commit

---
+++ assets/FormInput-BR6tTzwd.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-seHo-2Pj.js";import{I as a}from"./InputLabel-lrheF2GI.js";import{T as e,I as o}from"./TextInput-DWkgGXts.js";const x=({label:m,errorMessage:r,...t})=>s.jsxs("div",{children:[s.jsx(a,{htmlFor:t.name,value:m}),s.jsx(e,{className:"mt-1 block w-full",...t}),s.jsx(o,{className:"mt-2",message:r})]});export{x as F};
