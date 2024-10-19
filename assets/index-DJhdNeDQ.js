836f6a25d6f65255ce8056670d2094c17aea4305
Weerapat1993 <weerapat.thawatchoketawee@gmail.com>
2024-10-19 23:11:16 +0700

    Update v1.2.0

---
+++ assets/index-DJhdNeDQ.js
@@ -0,0 +1,1 @@
+import{r as s,a_ as u}from"./app-BuAjSH8i.js";import{e}from"./AuthenticatedLayout-DSELhlAX.js";var f=u.useId||(()=>{}),n=0;function d(t){const[r,a]=s.useState(f());return e(()=>{t||a(o=>o??String(n++))},[t]),t||(r?`radix-${r}`:"")}export{d as u};
