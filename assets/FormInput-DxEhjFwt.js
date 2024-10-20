fdbdc21d87cfc901102fe87c7485ff89fe68b78b
Weerapat1993 <weerapat.thawatchoketawee@gmail.com>
2024-10-20 15:49:46 +0700

    Update v1.2.2

--- assets/FormInput-DxEhjFwt.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-lPWvVKsc.js";import{I as a}from"./InputLabel-BCFStFdE.js";import{T as e,I as o}from"./TextInput-Dn7eC7lC.js";const x=({label:m,errorMessage:r,...t})=>s.jsxs("div",{children:[s.jsx(a,{htmlFor:t.name,value:m}),s.jsx(e,{className:"mt-1 block w-full",...t}),s.jsx(o,{className:"mt-2",message:r})]});export{x as F};
--- assets/index-B9v74Ff8.js
+++
@@ -1,1 +0,0 @@
-import{r as s,a_ as u}from"./app-lPWvVKsc.js";import{e}from"./AuthenticatedLayout-B5XN7Ces.js";var f=u.useId||(()=>{}),n=0;function d(t){const[r,a]=s.useState(f());return e(()=>{t||a(o=>o??String(n++))},[t]),t||(r?`radix-${r}`:"")}export{d as u};
